a1637eb1cea8ce55adeae768695fe724c4ceee19
Shadey <76909479+Shadyyey@users.noreply.github.com>
2021-09-25 17:40:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const Canvas = require("canvas");
+const { Canvas } = require("canvas");
+
 // Register Bold font
 Canvas.registerFont(`${__dirname}/assets/fonts/theboldfont.ttf`, { family: "Bold" });
+
 // Register SketchMatch font
 Canvas.registerFont(`${__dirname}/assets/fonts/SketchMatch.ttf`, { family: "SketchMatch" });
+
 // Register SketchMatch font
 Canvas.registerFont(`${__dirname}/assets/fonts/LuckiestGuy-Regular.ttf`, { family: "luckiest guy" });
+
 // Register KeepCalm font
 Canvas.registerFont(`${__dirname}/assets/fonts/KeepCalm-Medium.ttf`, { family: "KeepCalm" });
 
+// Exporting Modules
 module.exports.Base = require('./src/greetings/Base');
 module.exports.Welcome = require('./src/greetings/Welcome');
 module.exports.Goodbye = require('./src/greetings/Goodbye');
