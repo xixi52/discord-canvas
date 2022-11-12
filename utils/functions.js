9ead863520b6244b72424ab937f69cd7946f89f5
Emile2020 <62842896+Emile2020@users.noreply.github.com>
2022-11-12 16:47:53 +0100

    Changed formatVariable function
    Now it will not try to lowercase a number

--- utils/functions.js
+++ utils/functions.js
@@ -7,9 +7,13 @@ module.exports = {
      * @returns The variable formatted
      */
     formatVariable(prefix, variable){
+        if (typeof variable = "string) {
         const formattedVariable = variable.toLowerCase()
         .split("-").map((word) => word.charAt(0).toUpperCase()+word.substr(1, word.length).toLowerCase()).join("");
         return prefix+formattedVariable;
+    } else {
+       const formattedVariable = variable;
+    return prefix+formattedVariable;
     },
     
     /**
